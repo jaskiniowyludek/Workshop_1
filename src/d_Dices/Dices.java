4003ce52071533cb832fe142f8b91c1d91870df8
Ewelina Pasternak <wulina@wp.pl>
2018-09-05 13:55:46 +0200

    getDiceType method added

--- src/d_Dices/Dices.java
+++ src/d_Dices/Dices.java
@@ -4,8 +4,11 @@ import java.util.StringTokenizer;
 
 public class Dices {
     public static void main(String[] args) {
+        String test1 = "13D100-10";  //w głównej metody zrobić String diceCode toUpperCase!!
+        char[] test1Arr = test1.toCharArray();
+        System.out.println(getDiceType(test1Arr));
 
-        getDicesPoints("D100-10");
+      //  getDicesPoints("13D8-10");
     }
     static int getDicesPoints(String diceCode){
         int points = 0;
@@ -26,5 +29,40 @@ public class Dices {
         char[] diceCodeParts = diceCode.toCharArray();
 
         return points;
+    }
+
+    static String getDiceType(char[] parts){
+
+        String diceType="";
+      //  String[] dices = {"D3", "D4", "D6", "D8", "D10", "D12", "D20", "D100"};
+        int indexOfD = 0;
+        int counter = 0;
+        for (int i = 0; i<parts.length;i++){
+            if (parts[i]=='D'){
+                counter++;
+            }
+        }
+        if (counter<1|| counter>1){
+            System.out.println("The code you introduced is not correct!");
+        }else {
+            for (int i =0; i<parts.length;i++){
+                if (parts[i]=='D'){
+                    indexOfD=i;
+                }
+            }
+            for (int i =indexOfD;i<parts.length;i++){
+                diceType= diceType + String.valueOf(parts[i]);
+            }
+            StringTokenizer st = new StringTokenizer(diceType,"+-");
+            diceType=st.nextToken();
+            if (!(diceType.equals("D3")||diceType.equals("D4")||diceType.equals("D6")|| diceType.equals("D8")
+            ||diceType.equals("D10")||diceType.equals("D12")||diceType.equals("D20")||diceType.equals("D100"))){
+                diceType="";
+                System.out.println("The code you introduced is not correct!");
+                }
+            }
+
+
+        return diceType;
     }
 }
